768eb9f9df851c53c81e7d5a19df23768214ceb9
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-03-10 23:48:31 +0000

    Able to calculate summary data for given segments by index.

--- DataAnalysis/DiffusionParameterData.py
+++ DataAnalysis/DiffusionParameterData.py
@@ -1,36 +1,66 @@
 import pandas as pd
+import numpy as np
 
-column_ending = "_12_seg"
+column_ending = '_12_seg'
+supported_diffusion_parameters = ['E1', 'E2', 'E3', 'FA', 'MD', 'MODE',
+                                  'HA', 'E2A', 'IA']
+
+
+def __flatten__(list):
+    return [item for sublist in list for item in sublist]
 
 
 class DiffusionParameterData:
     def __init__(self, data):
-        self.data = {key.replace(column_ending, ""): value for (key, value) in data.items() if
-                     key.endswith(column_ending)}
-        self.diffusionparameters = {key: self.getparameter(key) for key in self.data.keys()}
+        self.raw_data = {key.replace(column_ending, ""): value for (key, value) in data.items() if
+                         key.endswith(column_ending)}
+        self.diffusion_parameters = {key: self.get_parameter(key) for key in self.raw_data.keys() if
+                                     key in supported_diffusion_parameters}
+
+        self.summary = self.get_summary_segments(range(0, 12))
 
     def __call__(self):
-        return self
+        return self.summary
 
-    def getparameter(self, param_name):
-        data = self.data[param_name]
-        paramframe = pd.DataFrame(data[-1])
-        return paramframe
+    def get_parameter(self, param_name):
+        data = self.raw_data[param_name]
+        param_frame = pd.DataFrame(data[-1])
+        return param_frame
 
-    def getparametermean(self, param_name):
-        param = self.diffusionparameters[param_name]
-        if 'mean' in param.columns:
-            return param['mean']
+    def calculate_parameter_means(self, param_name):
+        diffusion_param = self.diffusion_parameters[param_name]
+        if 'mean' in diffusion_param.columns:
+            return diffusion_param['mean']
         else:
             for i in range(0, 12):
-                param.at[i, 'mean'] = (param.at[i, 'values'].mean())
-            return param['mean']
+                diffusion_param.at[i, 'mean'] = (diffusion_param.at[i, 'values'].mean())
+            return diffusion_param['mean']
 
-    def getparametersd(self, param_name):
-        param = self.diffusionparameters[param_name]
-        if 'sd' in param.columns:
-            return param['sd']
+    def calculate_parameter_sds(self, param_name):
+        diffusion_param = self.diffusion_parameters[param_name]
+        if 'sd' in diffusion_param.columns:
+            return diffusion_param['sd']
         else:
             for i in range(0, 12):
-                param.at[i, 'sd'] = (param.at[i, 'values'].std())
-            return param['sd']
+                diffusion_param.at[i, 'sd'] = (diffusion_param.at[i, 'values'].std())
+            return diffusion_param['sd']
+
+    def get_parameter_segment_values(self, param_name, segments):
+        diffusion_param = self.diffusion_parameters[param_name]
+        values = []
+        for i in segments:
+            values.append(diffusion_param.at[i, 'values'][0].tolist())
+        return np.array(__flatten__(values))
+
+    def get_parameter_segment_summary(self, param_name, segments):
+        diffusion_param_values = self.get_parameter_segment_values(param_name, segments)
+        param_values_as_series = pd.Series(diffusion_param_values)
+        return [param_name, param_values_as_series.mean(), param_values_as_series.std()]
+
+    def get_summary_segments(self, segments):
+        columns = ['Diffusion Parameter', 'mean', 'standard deviation']
+        data = []
+        for parameter in self.diffusion_parameters.keys():
+            parameter_summary = self.get_parameter_segment_summary(parameter, segments)
+            data.append(parameter_summary)
+        return pd.DataFrame(data, columns=columns)
