087d6d52e026daed6cade1bebcbf4337bcc82c87
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2020-03-26 23:25:55 +0000

    Implemented erroneous file path handling, added method to create patient data summary interface.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,14 +1,15 @@
 import csv
 import io
-import sys
+import os
+from functools import partial
 
+from DataAnalysis import DataFrameModel as dfm, DiffusionParameterData as dpd
 from PyQt5 import QtWidgets, QtGui, QtCore
-from PyQt5.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QAbstractItemView, QButtonGroup, QCheckBox, \
-    QVBoxLayout, QHBoxLayout, QGroupBox
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QFileDialog, QPushButton, QAbstractItemView, QButtonGroup, QCheckBox, \
+    QVBoxLayout, QHBoxLayout, QGroupBox, QTableView, QLabel
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 from scipy.io import loadmat
-
-from DataAnalysis import DataFrameModel
-from DataAnalysis import DiffusionParameterData
 
 file_extension = '/result_images/exported_data/diffusion_parameters.mat'
 
@@ -20,14 +21,15 @@ class App(QWidget):
         self.title = 'Diffusion Parameter Results Viewer'
         self.left = 10
         self.top = 10
-        self.width = 700
-        self.height = 700
-        self.diffusion_parameters = DiffusionParameterData.DiffusionParameterData()
-        self.summary_table = QtWidgets.QTableView()
-        self.selected_region_summary_table = QtWidgets.QTableView()
+        self.width = 1920
+        self.height = 1080
+        self.diffusion_parameters = dpd.DiffusionParameterData()
+        self.summary_table = QTableView()
+        self.selected_region_summary_table = QTableView()
         self.region_selection_buttons = QButtonGroup()
         self.region_buttons_show = False
         self.vbox2 = QVBoxLayout()
+        self.hbox = QHBoxLayout()
         self.init_ui()
 
     #   Initialises UI
@@ -35,47 +37,70 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        #   Initialises UI
-        self.summary_table.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        self.summary_table.installEventFilter(self)
-
-        self.selected_region_summary_table.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        self.selected_region_summary_table.installEventFilter(self)
-        self.selected_region_summary_table.hide()
-
-        self.region_selection_buttons.setExclusive(False)
-
         load_file_button = QPushButton("Select File")
         load_file_button.move(100, 100)
         load_file_button.clicked.connect(self.open_file_dialog)
 
-        vbox1 = QVBoxLayout()
-        hbox1 = QHBoxLayout()
-
-        vbox1.addWidget(self.summary_table)
-        vbox1.addWidget(self.selected_region_summary_table)
         self.vbox2.addWidget(load_file_button)
         self.vbox2.addStretch(1)
-        hbox1.addLayout(vbox1)
-        hbox1.addLayout(self.vbox2)
+        self.hbox.addLayout(self.vbox2)
 
-        self.setLayout(hbox1)
+        self.setLayout(self.hbox)
         self.show()
 
+    # Method to add patient data summary to window
+    def display_patient_data(self, identifier, data):
+        vbox1 = QVBoxLayout()
+        vbox2 = QVBoxLayout()
+
+        patient_identifier_label = self.create_label(identifier, Qt.AlignCenter)
+        vbox1.addWidget(patient_identifier_label)
+
+        vbox1.addWidget(self.create_label("Diffusion Parameters Data Summary", Qt.AlignCenter))
+        summary_table = QTableView()
+        summary_table.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        summary_table.installEventFilter(self)
+        summary_table.setWindowTitle(identifier)
+        vbox1.addWidget(summary_table)
+
+        vbox1.addWidget(self.create_label("Selected Regions Summary", Qt.AlignCenter))
+        regions_summary_table = QTableView()
+        regions_summary_table.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        regions_summary_table.installEventFilter(self)
+        # regions_summary_table.hide()
+        vbox1.addWidget(regions_summary_table)
+
+        region_select_buttons = QButtonGroup()
+        region_select_buttons.setExclusive(False)
+
+        vbox2.addStretch(1)
+        self.create_region_selection(vbox2, region_select_buttons, regions_summary_table)
+        self.load_summary_table(data, summary_table)
+        self.hbox.addLayout(vbox1)
+        self.hbox.addLayout(vbox2)
+
+    def create_label(self, text, alignment):
+        label = QLabel()
+        label.setText(text)
+        label.setAlignment(alignment)
+        return label
+
     #   Creates region selection buttons
-    def create_region_selection(self):
+    def create_region_selection(self, widgetBox, buttonGroup, region_summary_table):
         box = QGroupBox("Regions")
         box.setStyleSheet("QGroupBox { border: 1px solid black;}")
         vbox = QVBoxLayout()
         vbox.addSpacing(20)
+        hbox = QHBoxLayout()
         for i in range(0, 12):
             check_box = QCheckBox(str(i + 1))
-            check_box.clicked.connect(self.update_selected_region_summary)
+            check_box.clicked.connect(partial(self.update_selected_region_summary, buttonGroup, region_summary_table))
             vbox.addWidget(check_box)
-            self.region_selection_buttons.addButton(check_box, i)
-        box.setLayout(vbox)
-        self.vbox2.addWidget(box)
-        self.vbox2.addStretch(1)
+            buttonGroup.addButton(check_box, i)
+        hbox.addLayout(vbox)
+        box.setLayout(hbox)
+        widgetBox.addWidget(box)
+        widgetBox.addStretch(1)
         self.region_buttons_show = True
 
     #   Creates region selection buttons
@@ -104,23 +129,20 @@ class App(QWidget):
             QtWidgets.qApp.clipboard().setText(stream.getvalue())
 
     #   Updates selected region summary table
-    def update_selected_region_summary(self):
-        segments = [i for i, button in enumerate(self.region_selection_buttons.buttons()) if button.isChecked()]
-        if len(segments) == 0:
-            self.selected_region_summary_table.hide()
-        else:
-            segment_summary = self.diffusion_parameters.get_regions_summary(segments)
-            model = DataFrameModel.DataFrameModel(segment_summary)
-            self.selected_region_summary_table.setModel(model)
-            self.selected_region_summary_table.resizeColumnsToContents()
-            self.selected_region_summary_table.show()
+    def update_selected_region_summary(self, region_buttons, region_summary_table):
+        segments = [i for i, button in enumerate(region_buttons.buttons()) if button.isChecked()]
+        segment_summary = self.diffusion_parameters.get_regions_summary(segments)
+        model = dfm.DataFrameModel(segment_summary)
+        region_summary_table.setModel(model)
+        region_summary_table.resizeColumnsToContents()
 
     #   Loads data summary
-    def load_summary_table(self, summary):
-        model = DataFrameModel.DataFrameModel(summary)
-        self.summary_table.setModel(model)
-        self.summary_table.resizeColumnsToContents()
-        self.summary_table.show()
+    def load_summary_table(self, data, summary_table):
+        model = dfm.DataFrameModel(data)
+        summary_table.setModel(model)
+        summary_table.resizeColumnsToContents()
+        summary_table.show()
+        return self.vbox2
 
     #   Allows user to open a file
     def open_file_dialog(self):
@@ -133,11 +155,7 @@ class App(QWidget):
             try:
                 data = loadmat(patient_data_directory + file_extension)
                 summary = self.diffusion_parameters.set_data(data)
-                self.load_summary_table(summary)
-                if not self.region_buttons_show:
-                    self.create_region_selection()
-                else:
-                    self.update_selected_region_summary()
+                self.display_patient_data(os.path.basename(patient_data_directory), summary)
             except:
                 error_dialog = QtWidgets.QErrorMessage()
                 error_dialog.showMessage(f'File path error: {patient_data_directory} does not contain diffusion '
@@ -146,6 +164,6 @@ class App(QWidget):
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
+    ctx = ApplicationContext()
     ex = App()
-    sys.exit(app.exec_())
+    ctx.app.exec_()
---
+++ src/main/resources/base/DiffusionParameterData.py
@@ -0,0 +1,94 @@
+import numpy as np
+import pandas as pd
+
+column_ending = '_12_seg'
+supported_diffusion_parameters = ['E1', 'E2', 'E3', 'FA', 'MD', 'MODE',
+                                  'HA', 'E2A', 'IA']
+
+
+def __flatten__(list):
+    return [item for sublist in list for item in sublist]
+
+
+# Data analysis class for diffusion parameter data
+class DiffusionParameterData:
+    def __init__(self):
+        self.data = dict
+        self.diffusion_parameters = dict
+        self.summary = pd.DataFrame()
+
+    def __call__(self):
+        return self.summary
+
+    # Sets class members
+    def set_data(self, data):
+        self.data = {key.replace(column_ending, ""): value for (key, value) in data.items() if
+                     key.endswith(column_ending)}
+        self.diffusion_parameters = {key: self.get_diffusion_parameter(key) for key in self.data.keys() if
+                                     key in supported_diffusion_parameters}
+        self.summary = self.get_regions_summary(range(0, 12))
+        return self.summary
+
+    # Gets diffusion parameter
+    def get_diffusion_parameter(self, param_name):
+        data = self.data[param_name]
+        param_frame = pd.DataFrame(data[-1])
+        return param_frame
+
+    # Updates current data to store means for each diffusion parameter
+    def calculate_parameter_means(self, param_name):
+        diffusion_param = self.diffusion_parameters[param_name]
+        if 'mean' in diffusion_param.columns:
+            return diffusion_param['mean']
+        else:
+            for i in range(0, 12):
+                diffusion_param.at[i, 'mean'] = (diffusion_param.at[i, 'values'].mean())
+            return diffusion_param['mean']
+
+    # Updates current data to store sd for each diffusion parameter
+    def calculate_parameter_sds(self, param_name):
+        diffusion_param = self.diffusion_parameters[param_name]
+        if 'sd' in diffusion_param.columns:
+            return diffusion_param['sd']
+        else:
+            for i in range(0, 12):
+                diffusion_param.at[i, 'sd'] = (diffusion_param.at[i, 'values'].std())
+            return diffusion_param['sd']
+
+    # Returns flat list of collective values for a given parameter and specified regions
+    def get_parameter_regions_values(self, param_name, regions):
+        diffusion_param = self.diffusion_parameters[param_name]
+        values = []
+        for i in regions:
+            values.append(diffusion_param.at[i, 'values'][0].tolist())
+        return np.array(__flatten__(values))
+
+    # Returns a summary triple for a given parameter and specified regions
+    def get_parameter_regions_summary(self, param_name, regions):
+        diffusion_param_values = self.get_parameter_regions_values(param_name, regions)
+        if param_name == "E2A":
+            diffusion_param_values = np.absolute(diffusion_param_values)
+
+        # Calculate data points
+        param_values_as_series = pd.Series(diffusion_param_values)
+        mean = param_values_as_series.mean()
+        std = param_values_as_series.std()
+        quartiles = param_values_as_series.quantile([.25, .5, .75]).to_numpy()
+        summary = [param_name, mean, std, quartiles[1], quartiles[2] - quartiles[0], quartiles[0], quartiles[2]]
+
+        # Scale if necessary
+        scale_params = ['E1', 'E2', 'E3']
+        if param_name in scale_params:
+            for i in range(1, len(summary)):
+                summary[i] = summary[i] * 1000
+        return summary
+
+    # Returns a summary panda data frame for all diffusion parameters in the given regions
+    def get_regions_summary(self, regions):
+        columns = ['Diffusion Parameter', 'Mean', 'Standard Deviation', 'Median', 'Interquartile Range',
+                   'Lower Quartile', 'Upper Quartile']
+        data = []
+        for parameter in self.diffusion_parameters.keys():
+            parameter_summary = self.get_parameter_regions_summary(parameter, regions)
+            data.append(parameter_summary)
+        return pd.DataFrame(data, columns=columns)
