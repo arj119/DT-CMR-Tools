8a15a07edea2cce9223d53043607562346539533
Pedro Ferreira <pedro.filipe@outlook.com>
2022-01-18 22:01:47 +0000

    start-command more generic
    Added installation instructions

---
+++ DT-CMR_RAT_app.command
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Add correct path to the main.py below
+
+cd "/Users/physics/Documents/Pedro/github/DT-CMR-Tools/"
+source "env/bin/activate"
+python "src/main/python/main.py"
--- start_rat.sh
+++
@@ -1,5 +0,0 @@
-# 1 - Add correct path to the main.py below
-# 2 - run from terminal source start_rat.sh
-
-conda activate rat_app
-python ~/Documents/Pedro/github/DT-CMR-Tools_adrian/src/main/python/main.py
